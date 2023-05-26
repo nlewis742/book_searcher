125a2cd50f1c64551e6dd8d6bbab16c06ffb502b
Natasha <nlackas12@gmail.com>
2023-05-25 20:02:23 -0500

    mongoose not working, but have main page loading with search

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,40 @@
+const { User, Book } = require('../models');
+
+const resolvers = {
+
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        }
+    }
+}
+
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+
+    type User {
+        _id: ID,
+        username: String,
+        email: String,
+        bookCount: Int,
+        savedBooks: [Book]
+    }
+
+    type Book {
+        _id: ID,
+        authors: [String],
+        description: String,
+        bookId: String,
+        image: String,
+        link: String,
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
+        saveBook()
+        removeBook()
+    }
+
+`;
+
+
+module.exports = typeDefs;
